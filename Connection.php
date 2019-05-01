3c93031a2a74ff6123b6c18d00df770fa1c8115a
AymericMenager <aymeric.menager@edu.ece.fr>
2019-05-01 15:20:00 +0200

    Base du site avec front end fini

--- Connection.php
+++ Connection.php
@@ -1,75 +1,72 @@
+
+
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Amazon ECE</title>
 </head>
-
 <body>
-    <?php
-    include 'PhpFunctions.php';
-    //compteur utilisé pour vérifier que l'utilisateur est enregistré dans la bdd
-    $connected = 0;
+<?php 
+include 'PhpFunctions.php';
+//compteur utilisé pour vérifier que l'utilisateur est enregistré dans la bdd
+$connected=0;
 
-    //connection a la base de données
-    $conn = ConnectDatabase();
-    $sql = "SELECT * FROM users";
-    $result = $conn->query($sql);
+$conn=ConnectDatabase();
+$sql="SELECT * FROM users";
+        $result = $conn->query($sql);
 
-    if (isset($result->num_rows)) {
-            if ($result->num_rows > 0) {
-
-                // parcourt de chaque ligne
-                while ($row = $result->fetch_assoc()) {
-                    if ($row["Email"] == $_POST["email"]) {
-                            if ($row["Password"] == $_POST["pwd"]) {
-                                    $_SESSION["user"] = $_POST["email"];
-                                    $_SESSION["type"] = $row["Type"];
-                                    echo "<center>" . $_POST["email"] . " est connecté comme un ";
-                                    switch ($_SESSION["type"]) {
-                                        case 0:
-                                            echo "client.";
-                                            break;
-                                        case 1:
-                                            echo "vendeur.";
-                                            break;
-                                        case 2:
-                                            echo "admin.";
-                                            break;
-                                    }
-                                    echo "<br><a href='index.html'>Home Page</a></center>";
-                                    $connected += 1;
-                                    break;
-                                } else {
-                                    echo "<center><div >Wrong password<br>Try again<br><a href='index.html'>Home Page</a></div></center>";
-                                    $connected += 1;
-                                    break;
-                                }
-                        }
+    if ($result->num_rows > 0) {
+    
+    // parcourt de chaque ligne
+    while($row = $result->fetch_assoc()) {
+        if($row["Email"]==$_POST["email"])
+            {
+                if($row["Password"]==$_POST["pwd"])
+                {
+                    $_SESSION["user"]=$_POST["email"];
+                    $_SESSION["type"]=$row["Type"];
+                    echo "<center>".$_POST["email"]." est connecté comme un ";
+                    switch($_SESSION["type"])
+                    {
+                        case 0: 
+                            echo "client.";
+                            break;
+                        case 1: 
+                            echo "vendeur.";
+                            break;
+                        case 2: 
+                            echo "admin.";
+                            break;
+                    }
+                    echo "<br><a href='HomePage.php'>Home Page</a></center>";
+                    $connected+=1;
+                    break;
                 }
-                if ($connected == 0)
-                    echo "<center><div >Your pseudo does not exists<br>Please create an account<br><a href='index.html'>Home Page</a></div></center>";
-            } else
-                echo "<center><div>There is not any account yet<br>Please create an account<br><a href='index.html'>Home Page</a></div></center>";
-        }
-        else
-            echo"<center><div>There is not any account on the database yet<br>Please create an account<br><a href='index.html'>Home Page</a></div></center>";
+                else
+                {
+                    echo "<center><div >Wrong password<br>Try again<br><a href='index.html'>Home Page</a></div></center>";
+                    $connected+=1;
+                    break;
+                }
 
-    $conn->close();
-
-
-
-
-
+                
+            }
+    }
+    if($connected==0)
+        echo "<center><div >Your pseudo does not exists<br>Please create an account<br><a href='index.html'>Home Page</a></div></center>";
 
 
+}
+else 
+    echo "<center><div>There is not any account yet<br>Please create an account<br><a href='index.html'>Home Page</a></div></center>";
 
-    ?>
+$conn->close();
 
+?>
 
+    
 </body>
-
 </html>
--- ProductPage.php
+++ ProductPage.php
@@ -2,61 +2,129 @@
 <html>
 <head>
 <title>ECE Amazon</title>
+<meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-<script src="Dynamism.js"></script>
+<link href="//netdna.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" type="text/css" href="HomePage.css">
+<link rel="stylesheet" type="text/css" href="ProductPageBis.css">
+
 </head>
 <body>
 
+<!------ HEADER NAVBAR ---------->
 <div id="header_color">
-        <img src="logoeceamazon_bisbis.png">
-        <center><INPUT TYPE=text name=q size=50 maxlength=255 value=""> 
-        <INPUT type=submit name=btnG VALUE="Search on this website"></center>         
-</div>
+  <img src="res/logoeceamazon_bisbis.png">  
+    <span align="center"><INPUT TYPE=text name=q size=50 maxlength=255 value=""> 
+    <INPUT type=submit name=btnG VALUE="Search on this website"></span>
   
-<br>
-<div class="navbar">
-<a href="HomePageNew">Accueil</a>
-  <div class="dropdown">
-    <button class="dropbtn">Categories 
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown-content">
-    <a href="SellingPage.php?Cat=0" id="livres">Livres</p>
-      <a href="SellingPage.php?Cat=1" id="music">Musique</a>
-      <a href="SellingPage.php?Cat=2" id="vetement">V&ecirctement</a>
-      <a href="SellingPage.php?Cat=3" id="sport">Sport & Loisirs</a>
+  <br>
+    <div class="navbar">
+      <a href="HomePage.php">Accueil</a>
+      <div class="dropdown">
+        <button class="dropbtn">Categories 
+          <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content">
+        <a href="SellingPage.php?Cat=0" id="livres">Livres</a>
+          <a href="SellingPage.php?Cat=1" id="music">Musique</a>
+          <a href="SellingPage.php?Cat=2" id="vetement">V&ecirctement</a>
+          <a href="SellingPage.php?Cat=3" id="sport">Sport & Loisirs</a>
+        </div>
+      </div> 
+
+      <a href="SellProductPage.php">Vendre</a>
+      <div class="dropdown">
+        <button class="dropbtn">Admin 
+          <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content">
+          <a href="AdminPage.php">Gérer les vendeurs</a>
+          <a href="AdminPage.php">Gérer les produits</a>
+        </div>
+      </div>
+      <a href="PanierPage.php">Panier</a>
+      <a href="MyAccount.php">Mon Compte</a>
+      <a href="Login.php">Se Connecter</a>
+  </div>
+</div>
+
+
+
+
+<?php
+					include "PhpFunctions.php";
+
+					//DATABASE
+					$conn=ConnectDatabase();
+					
+					//si le BDD existe, faire le traitement
+					$conn->set_charset('utf8');
+					$conn->query("SET character_set_results = 'utf8', character_set_client = 'utf8', character_set_connection = 'utf8', character_set_database = 'utf8', character_set_server = 'utf8'");
+					$sql = "SELECT * FROM products WHERE ID=".$_GET['Id'];
+          $result = $conn->query( $sql);
+?>
+
+<!------ CONTAINER BODY ---------->
+<div class="container">
+    <div id="bloc_sup">
+    <table>
+        <tr>
+            <td><div id="img_bloc">Image</div></td>
+            <td valign="top">
+            <?php while ($data = mysqli_fetch_assoc($result)) {  
+            echo "<div id='format_title'><div class=product-title>".$data['Name']."</div></div>
+                <div id='format_prix'>".$data['Price']." €</div>
+                <div id='format_stock'>In Stock</div>
+                <div id='format_btn'>
+                <div class='btn-group cart'>
+							<button type='button' class='btn btn-success' id='addToCart'>
+								Ajouter au panier
+						</button>
+                </div>
+</div>
+        </table>
+</div>
+    <br><br><div id='desc'><h2>Description</h2><br>
+    ".$data['Descr']."
+    Novo denique perniciosoque exemplo idem Gallus ausus est inire flagitium grave, 
+    quod Romae cum ultimo dedecore temptasse aliquando dicitur Gallienus, 
+    et adhibitis paucis clam ferro succinctis vesperi per tabernas palabatur et conpita quaeritando Graeco sermone,
+     cuius erat inpendio gnarus, quid de Caesare quisque sentiret. 
+     et haec confidenter agebat in urbe ubi pernoctantium luminum claritudo dierum solet imitari fulgorem. 
+     postremo agnitus saepe iamque, si prodisset, conspicuum se fore contemplans, non nisi luce palam egrediens ad agenda quae putabat seria cernebatur. et haec quidem medullitus multis gementibus agebantur.
+</div></div>
+";}
+//fermer la connection
+$conn->close();?>
+
+<!------ FOOTER ---------->
+<footer class="page-footer">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 col-md-8 col-sm-12">
+        <h6 class="text-uppercase font-weight-bold">Information additionnelle</h6>
+        <p>
+          Sed aliquet dolor id sapien rutrum, id vulputate quam iaculis.
+        </p>
+        <p>
+          Sed aliquet dolor id sapien rutrum, id vulputate quam iaculis.
+        </p>
+      </div>
+      <div class="col-lg-4 col-md-4 col-sm-12">
+        <h6 class="text-uppercase font-weight-bold">Contact</h6>
+        <p>
+          37, quai de Grenelle, 75015 Paris, France <br>
+          info@webDynamique.ece.fr <br>
+        </p>
+      </div>
     </div>
-  </div> 
-
-  <a href="SellProductPage">Vendre</a>
-  <a href="AdminPage">Admin</a>
-    <div id="text_nav">
-  <a href="PanierPage.php">Panier</a>
-  <a href="#">Mon Compte</a>
-  <a href="Login.php">Se Connecter</a>
-
-</div>
-</div>
-
-<h1></h1>
-
-<script type="text/javascript">
-    $(document).ready(function () {
-        $('.header').height($(window).height());
-    });
-</script>
-
-<footer>
-        <small>37, quai de Grenelle, 75015 Paris, France
-        &copy; 2019 Copyright | Droit d'auteur: webDynamique.ece.fr</small>
+  </div>
+  <div class="footer-copyright text-center">&copy; 2019 Copyright | Droit d'auteur: webDynamique.ece.fr</div>
 </footer>
-
-</body>
-
-
 
 </body>
 </html>
