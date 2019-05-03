bea7125e02f806772d1e4340c8310b84f97945d8
AymericMenager <aymeric.menager@edu.ece.fr>
2019-05-03 19:09:42 +0200

    Update HomePage.php

--- HomePage.php
+++ HomePage.php
@@ -10,85 +10,99 @@
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" type="text/css" href="HomePage.css">
+<link rel="stylesheet" type="text/css" href="HomePageBody.css">
 </head>
 <body>
 
 
 <!------ HEADER NAVBAR ---------->
-<div id="header_color">
-  <img src="res/logoeceamazon_bisbis.png">  
-    <span align="center"><INPUT TYPE=text name=q size=50 maxlength=255 value=""> 
-    <INPUT type=submit name=btnG VALUE="Search on this website"></span>
-  
-  <br>
-    <div class="navbar">
-      <a href="HomePage.php">Accueil</a>
-      <div class="dropdown">
-        <button class="dropbtn">Categories 
-          <i class="fa fa-caret-down"></i>
-        </button>
-        <div class="dropdown-content">
-        <a href="SellingPage.php?Cat=0" id="livres">Livres</a>
-          <a href="SellingPage.php?Cat=1" id="music">Musique</a>
-          <a href="SellingPage.php?Cat=2" id="vetement">V&ecirctement</a>
-          <a href="SellingPage.php?Cat=3" id="sport">Sport & Loisirs</a>
-        </div>
-      </div> 
-
-      <a href="SellProductPage.php">Vendre</a>
-
-      <div class="dropdown">
-        <button class="dropbtn">Admin 
-          <i class="fa fa-caret-down"></i>
-        </button>
-        <div class="dropdown-content">
-        <a href="AdminPage.php">Gérer les vendeurs</a>
-          <a href="AdminPage.php">Gérer les produits</a>
-        </div>
-      </div>
-
-      <a href="PanierPage.php">Panier</a>
-      <a href="MyAccount.php">Mon Compte</a>
-      <a href="Login.php">Se Connecter</a>
-      
-  </div>
-</div>
+<?php include("header.php"); ?>
 
 
 <!------ CONTAINER BODY ---------->
-<div class="container">
-  <h1>LE CONTENU DE LA PAGE</h1>
-  <script type="text/javascript">
-    $(document).ready(function () {
-      $('.header').height($(window).height());
-    });
-  </script>
+<?php
+	include "PhpFunctions.php";
+
+	//DATABASE
+	$conn=ConnectDatabase();
+					
+  //si le BDD existe, faire le traitement
+  $conn->set_charset('utf8');
+  $conn->query("SET character_set_results = 'utf8', character_set_client = 'utf8', character_set_connection = 'utf8', character_set_database = 'utf8', character_set_server = 'utf8'");
+  $sql = "SELECT * FROM products WHERE TauxPromo !=0";
+  $result = $conn->query( $sql);
+  ?>
+
+<h1>PROMOTION ET VENTES FLASH</h1>
+<div id="promo">
+  <table>
+    <tr>
+      <?php
+      while ($data = mysqli_fetch_assoc($result)) {  
+          echo "
+          <td>
+            <div id='bloc_produit'>
+              <div id='img_product'>
+                Image produit
+              </div>
+              <table>
+              <tr>
+                <td>
+                  <a href='ProductPage.php?Id=".$data['ID']."'><div id='format_title'>".$data['Name']."</div></a>
+                </td>
+                <td>
+                  <div id='format_promo'>
+                    PROMO
+                  </div>
+                  -".$data['TauxPromo']."%
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <div id='format_desc'>".$data['Descr']."</div>
+                </td>
+              </tr>
+              </table>
+            </div>
+          </td>";
+        }
+      ?>
+    </tr>
+  </table>
 </div>
 
+<h1>BEST SELLER</h1>
+<div id="bottom">
+  <table>
+    <tr>
+      <?php
+        for($i=0; $i<4; $i++)
+        {
+          echo "
+          <td>
+            <div id='bloc_produit'>
+            <div id='img_product'>
+            Image produit
+            </div>
+              <div id='format_description'>Description</div>
+            </div>
+          </td>";
+        }
+      ?>
+    </tr>
+  </table>
+</div>
+
+
+<script type="text/javascript">
+  $(document).ready(function () {
+    $('.header').height($(window).height());
+  });
+</script>
+
+
 <!------ FOOTER ---------->
-<footer class="page-footer">
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-8 col-md-8 col-sm-12">
-        <h6 class="text-uppercase font-weight-bold">Information additionnelle</h6>
-        <p>
-          Sed aliquet dolor id sapien rutrum, id vulputate quam iaculis.
-        </p>
-        <p>
-          Sed aliquet dolor id sapien rutrum, id vulputate quam iaculis.
-        </p>
-      </div>
-      <div class="col-lg-4 col-md-4 col-sm-12">
-        <h6 class="text-uppercase font-weight-bold">Contact</h6>
-        <p>
-          37, quai de Grenelle, 75015 Paris, France <br>
-          info@webDynamique.ece.fr <br>
-        </p>
-      </div>
-    </div>
-  </div>
-  <div class="footer-copyright text-center">&copy; 2019 Copyright | Droit d'auteur: webDynamique.ece.fr</div>
-</footer>
+<?php include("footer.php"); ?>
 
 
 
