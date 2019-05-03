4c245f7612998b7a85591cc9470c8627e8041a99
AymericMenager <aymeric.menager@edu.ece.fr>
2019-05-03 10:20:27 +0200

    Update Panier, ProductList and SellingPage (php and css)

--- ProductPage.php
+++ ProductPage.php
@@ -10,9 +10,21 @@
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" type="text/css" href="HomePage.css">
-<link rel="stylesheet" type="text/css" href="ProductPage.css">
+<link rel="stylesheet" type="text/css" href="ProductPageBis.css">
+
+<script>
+  function addToCart(){
+    document.location.href="ProductPage.php?Id="+<?php echo $_GET['Id']; ?>+"&Ajout=1";
+  }
+</script>
+
+
+
+
 </head>
 <body>
+
+
 
 <!------ HEADER NAVBAR ---------->
 <div id="header_color">
@@ -51,85 +63,84 @@
   </div>
 </div>
 
+
 <!------ CONTAINER BODY ---------->
-<div class="container-fluid">
-  <div class="content-wrapper">	
-		<div class="item-container">	
-			<div class="container">	
-				<div class="col-md-12">
-					<div class="product col-md-3 service-image-left">      
-						<center>
-							<img id="item-display" src="res/image_product/france4.jpg" alt=""></img>
-						</center>
-					</div>
+
+<!-- Mise en place de la connexion et requete SQL -->
+<?php
+					include "PhpFunctions.php";
+
+					//DATABASE
+					$conn=ConnectDatabase();
 					
-					<div class="container service1-items col-sm-2 col-md-2 pull-left">
-						<center>
-							<a id="item-1" class="service1-item">
-								<img src="res/image_product/france1.jpg" alt=""></img>
-							</a>
-							<a id="item-2" class="service1-item">
-								<img src="res/image_product/france2.jpg" alt="hoho"></img>
-							</a>
-							<a id="item-3" class="service1-item">
-								<img src="res/image_product/france3.jpg" alt=""></img>
-							</a>
-						</center>
-					</div>
-				</div>
-	
-				
-			</div>
-		</div> 
-	</div>
-		<div class="container-fluid">		
-			<div class="col-md-12 product-info">
-				<ul id="myTab" class="nav nav-tabs nav_tabs">
-						
-					<li class="active"><a href="#service-one" data-toggle="tab">DESCRIPTION</a></li>
-					
-						
-				</ul>
-				<div id="myTabContent" class="tab-content">
-					<div class="tab-pane fade in active" id="service-one">
-						<section class="container product-info">
-								
-							The Corsair Gaming Series GS600 power supply is the ideal price-performance solution for building or upgrading a Gaming PC. A single +12V rail provides up to 48A of reliable, continuous power for multi-core gaming PCs with multiple graphics cards. The ultra-quiet, dual ball-bearing fan automatically adjusts its speed according to temperature, so it will never intrude on your music and games. Blue LEDs bathe the transparent fan blades in a cool glow. Not feeling blue? You can turn off the lighting with the press of a button.
+					//si le BDD existe, faire le traitement
+					$conn->set_charset('utf8');
+					$conn->query("SET character_set_results = 'utf8', character_set_client = 'utf8', character_set_connection = 'utf8', character_set_database = 'utf8', character_set_server = 'utf8'");
+					$sql = "SELECT * FROM products WHERE ID=".$_GET['Id'];
+          $result = $conn->query( $sql);
+?>
+<script>
+    console.log(<?php echo "'".$_GET['Id']."'"; ?>);
+    console.log('<?php
+    if (isset($_SESSION['Panier']))
+    {
+      print_r( $_SESSION['Panier']); 
+    } 
+    ?>');
 
-								<h3>Corsair Gaming Series GS600 Features:</h3>
-								<li>It supports the latest ATX12V v2.3 standard and is backward compatible with ATX12V 2.2 and ATX12V 2.01 systems</li>
-								<li>An ultra-quiet 140mm double ball-bearing fan delivers great airflow at an very low noise level by varying fan speed in response to temperature</li>
-								<li>80Plus certified to deliver 80% efficiency or higher at normal load conditions (20% to 100% load)</li>
-								<li>0.99 Active Power Factor Correction provides clean and reliable power</li>
-								<li>Universal AC input from 90~264V — no more hassle of flipping that tiny red switch to select the voltage input!</li>
-								<li>Extra long fully-sleeved cables support full tower chassis</li>
-								<li>A three year warranty and lifetime access to Corsair’s legendary technical support and customer service</li>
-								<li>Over Current/Voltage/Power Protection, Under Voltage Protection and Short Circuit Protection provide complete component safety</li>
-								<li>Dimensions: 150mm(W) x 86mm(H) x 160mm(L)</li>
-								<li>MTBF: 100,000 hours</li>
-								<li>Safety Approvals: UL, CUL, CE, CB, FCC Class B, TÜV, CCC, C-tick</li>
-						</section>			  
-					</div>
-					<div class="tab-pane fade" id="service-two">
-						<section class="container">		
-						</section>
-					</div>
-					<div class="tab-pane fade" id="service-three">					
-					</div>
-				</div>
-				<hr>
-			</div>
-		</div>
-	</div>
-</div>
-
-
-<script type="text/javascript">
-    $(document).ready(function () {
-        $('.header').height($(window).height());
-    });
 </script>
+<div class="container">
+  <div id="bloc_sup">
+    <table>
+        <tr>
+            <td><div id="img_bloc">Image</div></td>
+            <td valign="top">
+              <?php while ($data = mysqli_fetch_assoc($result)) {
+                $price_promo = $data['Price'] *(1 - ($data['TauxPromo'])/100);
+                if($price_promo != $data['Price']){  
+              echo "<div id='format_title'><div class=product-title>".$data['Name']."</div></div>
+                    <div id='oldprix'><del>".$data['Price']." €</del></div>
+                    <div id='format_prix'>PROMOTION !!<br>".$price_promo." €</div>";}else{
+                      echo "<div id='format_title'><div class=product-title>".$data['Name']."</div></div>
+                      <div id='format_prix'>".$price_promo." €</div>";
+                      }
+                    echo "<div id='format_stock'>In Stock</div>
+                    <div id='format_btn'><div class='btn-group cart'>
+                    <button type='button' class='btn btn-success' id='addToCart' onclick='addToCart()'>
+                        Ajouter au panier
+                    </button>
+                    </div></div>
+        </tr>
+    </table>
+  </div>
+    <br><br><div id='desc'><h2>Description</h2><br>
+    ".$data['Descr']."
+    Novo denique perniciosoque exemplo idem Gallus ausus est inire flagitium grave, 
+    quod Romae cum ultimo dedecore temptasse aliquando dicitur Gallienus, 
+    et adhibitis paucis clam ferro succinctis vesperi per tabernas palabatur et conpita quaeritando Graeco sermone,
+     cuius erat inpendio gnarus, quid de Caesare quisque sentiret. 
+     et haec confidenter agebat in urbe ubi pernoctantium luminum claritudo dierum solet imitari fulgorem. 
+     postremo agnitus saepe iamque, si prodisset, conspicuum se fore contemplans, non nisi luce palam egrediens ad agenda quae putabat seria cernebatur. et haec quidem medullitus multis gementibus agebantur.
+  </div>
+</div>"
+;}
 
+//fermer la connection
+$conn->close();?>
+
+<?php
+ 
+ if(isset($_GET["Ajout"])&& $_GET["Ajout"]==1)
+ {
+   $_SESSION["Panier"][]=$_GET["Id"];
+   ?>
+   <script>
+   console.log(<?php echo "'".$_GET['Id']."'"; ?>);
+
+   </script>
+   <?php
+ }
+?>
 <!------ FOOTER ---------->
 <footer class="page-footer">
   <div class="container">
--- SellingPage.php
+++ SellingPage.php
@@ -15,42 +15,7 @@
 <body>
 
 <!------ HEADER NAVBAR ---------->
-<div id="header_color">
-  <img src="res/logoeceamazon_bisbis.png">  
-    <span align="center"><INPUT TYPE=text name=q size=50 maxlength=255 value=""> 
-    <INPUT type=submit name=btnG VALUE="Search on this website"></span>
-  
-  <br>
-    <div class="navbar">
-      <a href="HomePage.php">Accueil</a>
-      <div class="dropdown">
-        <button class="dropbtn">Categories 
-          <i class="fa fa-caret-down"></i>
-        </button>
-        <div class="dropdown-content">
-        <a href="SellingPage.php?Cat=0" id="livres">Livres</a>
-          <a href="SellingPage.php?Cat=1" id="music">Musique</a>
-          <a href="SellingPage.php?Cat=2" id="vetement">V&ecirctement</a>
-          <a href="SellingPage.php?Cat=3" id="sport">Sport & Loisirs</a>
-        </div>
-      </div> 
-
-      <a href="SellProductPage.php">Vendre</a>
-      <div class="dropdown">
-        <button class="dropbtn">Admin 
-          <i class="fa fa-caret-down"></i>
-        </button>
-        <div class="dropdown-content">
-          <a href="AdminPage.php">Gérer les vendeurs</a>
-          <a href="AdminPage.php">Gérer les produits</a>
-        </div>
-      </div>
-      <a href="PanierPage.php">Panier</a>
-      <a href="MyAccount.php">Mon Compte</a>
-      <a href="Login.php">Se Connecter</a>
-  </div>
-</div>
-
+<?php include("header.php"); ?>
 
 <!------ CONTAINER BODY ---------->
 <?php
@@ -75,9 +40,19 @@
                   <tr>
                     <td><div class='img_bloc'>Image</div></td>
                     <td valign='top'>
-                      <div class='format_title'><div class=product-title><a href='ProductPage.php?Id=".$data['ID']."'>".$data['Name']."</a></div></div>
-                      <div class='format_prix'>".$data['Price']." €</div>
-                      <div class='desc'>
+                      <div class='format_title'><div class=product-title><a href='ProductPage.php?Id=".$data['ID']."'>".$data['Name']."</a></div></div>";
+                      if($data['TauxPromo']!=0){
+                        echo "<table>
+                        <tr>
+                        <td><div class='format_prix'>".$data['Price']." €</div></td>
+                        <td><div class='format_prix'>-".$data['TauxPromo']."%</div></td>
+                        </tr>
+                        </table>";
+                              
+                      }else{
+                      echo"<div class='format_prix'>".$data['Price']." €</div>";}
+
+                      echo"<div class='desc'>
                         ".$data['Descr']."
                       </div>
                     </td>
@@ -90,6 +65,22 @@
       ?> 
   </div>
 </div>
+
+<script type="text/javascript">
+    $(document).ready(function() {
+            $('.bloc_produit').mouseover(function(){
+              $(this).css("background-color", "#ddd");
+              $(this).children().css("background-color", "#ddd");
+            $('.img_bloc').css("background-color", "white");
+              
+            });
+            $('.bloc_produit').mouseout(function(){
+              $(this).css("background-color", "white");
+              $(this).children().css("background-color", "white");
+            });
+        });
+</script>
+
 <script type="text/javascript">
     $(document).ready(function () {
         $('.header').height($(window).height());
